0a2ab0530950cff747395eafaad4eb4d58962f2a
light5551 <sergey.glazunov.99@mail.ru>
2019-05-27 04:43:57 +0300

    добавлена авторизация

--- web-service/src/app.py
+++ web-service/src/app.py
@@ -1,3 +1,6 @@
+import os
 from flask import Flask
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'VERY_SECRET_KEY'
--- web-service/src/view.py
+++ web-service/src/view.py
@@ -1,8 +1,9 @@
 from app import app
 from main import main as create_word
 from json_api import JsonApi as update_settings
-from flask import render_template, redirect, url_for
-from flask import request
+from flask import render_template, redirect, url_for, request
+from flask_dance.contrib.github import github
+
 link = ""
 @app.route('/', methods=["GET", 'POST'])
 @app.route('/home', methods=["GET", 'POST'])
@@ -16,4 +17,26 @@ def index():
         link = create_word([repo, wiki, branch])
         return redirect(url_for("index"))
 
-    return render_template("home.html", link=link)
+    current_github = ''
+    if github.authorized:
+        current_github = github.get('/user').json()
+    return render_template("home.html", link=link, github=current_github)
+
+@app.route('/github', methods=["GET", 'POST'])
+def github_login():
+    if not github.authorized and request.method == 'GET':
+        return redirect(url_for('github.login'))
+
+    if github.authorized:
+        account_info = github.get('/user')
+
+        if account_info.ok:
+            account_info_json = account_info.json()
+            return 'Your github name is {login}'.format(login=account_info_json['login'])
+            #return render_template('sign.html', github_nick=account_info_json['login'],
+			#image=account_info_json['avatar_url'])
+    #return render_template('sign.html')
+    return 'OOPS'
+
+
+
