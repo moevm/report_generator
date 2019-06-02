3ed934fe7bb3bbdecb282728f83460987da3c4e0
light5551 <sergey.glazunov.99@mail.ru>
2019-06-03 01:41:45 +0300

    переход на github-flask

--- web-service/src/start_service.py
+++ web-service/src/start_service.py
@@ -1,13 +1,6 @@
 #!../venv/bin/python3.6
 from app import app
-from flask_dance.contrib.github import make_github_blueprint
 import view
-
-CLIENT_ID = '6bfefa93bab199af589e'
-CLIENT_SECRET = '49c1cf2398a705a235a9411a2a8fa7f3d7c5e974'
-
-github_blueprint = make_github_blueprint(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-app.register_blueprint(github_blueprint, url_prefix='/login')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
--- web-service/src/view.py
+++ web-service/src/view.py
@@ -1,9 +1,15 @@
 from app import app
 from main import main as create_word
 from json_api import JsonApi as update_settings
-from flask import render_template, redirect, url_for, request
-from flask_dance.contrib.github import github
-import requests
+from flask import render_template, redirect, url_for, request, jsonify, g
+from flask_github import GitHub
+
+CLIENT_ID = '6bfefa93bab199af589e'
+CLIENT_SECRET = '49c1cf2398a705a235a9411a2a8fa7f3d7c5e974'
+
+app.config['GITHUB_CLIENT_ID'] = CLIENT_ID
+app.config['GITHUB_CLIENT_SECRET'] = CLIENT_SECRET
+github = GitHub(app)
 
 link = ""
 @app.route('/', methods=["GET", 'POST'])
@@ -20,12 +26,39 @@ def index():
 
     current_github = ''
     list_of_repo = []
-    if github.authorized:
-        current_github = github.get('/user').json()
-        list_of_repo = create_list_of_repo()
+    #if github.authorized:
+        #current_github = github.get('/user').json()
+        #list_of_repo = create_list_of_repo()
     return render_template("home.html", link=link, github=current_github, repositories=list_of_repo)
 
+@app.route('/login')
+def login():
+    return github.authorize(scope="user,repo,repo:status")
 
+@github.access_token_getter
+def token_getter():
+    return g.token
+
+@app.route('/login/github/authorized')
+@github.authorized_handler
+def user(acc):
+    print(acc)
+    g.token = acc
+
+    if acc:
+        gu = github.get('/user')
+        k = github.get('/users/light5551/repos')
+        return jsonify(k)
+        #print(github.get('/user').json())
+    #if github.get('/user'):
+    #    return (jsonify(github.get('/user')))
+    return 'fail'
+    #return jsonify(github.get('/user'))
+
+@app.route('/login/github/authorized')
+def check():
+    return 'ok'
+'''
 @app.route('/github', methods=["GET", 'POST'])
 def github_login():
     if not github.authorized and request.method == 'GET':
@@ -57,4 +90,4 @@ def create_list_of_repo():
             return list_of_repo
     return []
 
-
+'''
