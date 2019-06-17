5cb5822257ea48696485db6f3e02f463b38ecec5
light5551 <sergey.glazunov.99@mail.ru>
2019-06-17 17:21:13 +0300

    убрал глобальную переменную link

--- web-service/src/view.py
+++ web-service/src/view.py
@@ -1,19 +1,19 @@
 from app import app
 from main import main as create_word
 from json_api import JsonApi as update_settings
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session
 from flask import request
-link = ""
+
+
 @app.route('/', methods=["GET", 'POST'])
 @app.route('/home', methods=["GET", 'POST'])
 def index():
-    global link
     if request.method == 'POST':
         update_settings(dict(request.form))
         repo = request.form['repo_name']
         wiki = request.form['wiki_name']
         branch = request.form['branch_name']
-        link = create_word([repo, wiki, branch])
+        session['link'] = create_word([repo, wiki, branch])
         return redirect(url_for("index"))
 
-    return render_template("home.html", link=link)
+    return render_template("home.html", link=session.get('link'))
