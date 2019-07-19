f5d01cda2dece49dc4ea05dc508850038f7fa4d8
light5551 <sergey.glazunov.99@mail.ru>
2019-07-20 00:08:47 +0300

    fixed double send report in google drive

--- web-service/src/google/google_api.py
+++ web-service/src/google/google_api.py
@@ -1,108 +1,82 @@
 import os
 from app import app
 import flask
-import requests
+from flask import url_for, redirect
 
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 from googleapiclient.http import MediaFileUpload
 import googleapiclient.discovery
-
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+CREDENTIALS = 'credentials'
 CLIENT_SECRETS_FILE = "client_secrets.json"
-
+TITLE = 'title'
+NAME_OF_FILE = 'report.pdf'
+TYPE = 'mimeType'
+PDF = 'application/pdf'
+MEDIA_TYPE = 'text/pdf'
+ID = 'id'
+OFFLINE = 'offline'
+STATE = 'state'
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 API_SERVICE_NAME = 'drive'
 API_VERSION = 'v2'
 
-#app = flask.Flask(__name__)
 
-
-
-@app.route('/test')
-def test_api_request():
-    if 'credentials' not in flask.session:
-        return flask.redirect('authorize')
-
+@app.route('/post_report_pdf')
+def post_file_api_request():
+    if CREDENTIALS not in flask.session:
+        return flask.redirect(url_for('google_authorize'))
     credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials'])
-
+        **flask.session[CREDENTIALS])
     drive = googleapiclient.discovery.build(
         API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
-    file_metadata = {'title': 'report.pdf', 'mimeType': 'application/pdf'}
-    media = MediaFileUpload('report.pdf',
-                            mimetype='text/pdf')
+    file_metadata = {TITLE: NAME_OF_FILE, TYPE: PDF}
+    media = MediaFileUpload(NAME_OF_FILE,
+                            mimetype=MEDIA_TYPE)
     file = drive.files().insert(body=file_metadata,
                                 media_body=media,
-                                fields='id').execute()
+                                fields=ID).execute()
 
-    flask.session['credentials'] = credentials_to_dict(credentials)
+    flask.session[CREDENTIALS] = credentials_to_dict(credentials)
 
-    return flask.redirect(flask.url_for('index'))  # flask.jsonify(**files)
+    return redirect(url_for('index'))
 
 
 @app.route('/googleauthorize')
 def google_authorize():
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES)
+    if CREDENTIALS in flask.session:
+        return flask.redirect(url_for('post_file_api_request'))
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
 
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    flow.code_verifier = 'fdskfhjdshjdhghajdhgjhdfjgjfdgjhfjghfjhgfhfdhjssgjfdgsdfhghfdgjdfnjgajfd'
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    flow.code_verifier = os.urandom(50)
     authorization_url, state = flow.authorization_url(
-        access_type='offline',
+        access_type=OFFLINE,
         include_granted_scopes='true')
 
-    flask.session['state'] = state
+    flask.session[STATE] = state
 
-    return flask.redirect(authorization_url)
+    return redirect(authorization_url)
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    state = flask.session['state']
+    state = flask.session[STATE]
 
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=None,
-        state=state)
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    flow.code_verifier = 'fdskfhjdshjdhghajdhgjhdfjgjfdgjhfjghfjhgfhfdhjssgjfdgsdfhghfdgjdfnjgajfd'
+        CLIENT_SECRETS_FILE, scopes=None, state=state)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    flow.code_verifier = os.urandom(50)
 
     authorization_response = flask.request.url
     flow.fetch_token(authorization_response=authorization_response)
     credentials = flow.credentials
-    flask.session['credentials'] = credentials_to_dict(credentials)
+    flask.session[CREDENTIALS] = credentials_to_dict(credentials)
 
-    return flask.redirect(flask.url_for('test_api_request'))
-
-
-@app.route('/revoke')
-def revoke():
-    if 'credentials' not in flask.session:
-        return ('You need to <a href="/authorize">authorize</a> before ' +
-                'testing the code to revoke credentials.')
-
-    credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials'])
-
-    revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
-                           params={'token': credentials.token},
-                           headers={'content-type': 'application/x-www-form-urlencoded'})
-
-    status_code = getattr(revoke, 'status_code')
-    if status_code == 200:
-        return ('Credentials successfully revoked.' + print_index_table())
-    else:
-        return ('An error occurred.' + print_index_table())
-
-
-#@app.route('/clear')
-#def clear_credentials():
-#    if 'credentials' in flask.session:
-#        del flask.session['credentials']
-#    return 'clear'
+    return flask.redirect(url_for('post_file_api_request'))
 
 
 def credentials_to_dict(credentials):
@@ -112,6 +86,3 @@ def credentials_to_dict(credentials):
             'client_id': credentials.client_id,
             'client_secret': credentials.client_secret,
             'scopes': credentials.scopes}
-
-
-
