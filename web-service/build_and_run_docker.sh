724c0813cc2210ade5d1b146c34b97d8014f7c2a
light5551 <sergey.glazunov.99@mail.ru>
2019-08-07 16:34:17 +0300

    rm old container in docker

--- web-service/build_and_run_docker.sh
+++ web-service/build_and_run_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker rm -f report_generator
+docker rm -f report_generator_doc
 docker build -t report_generator_image . --no-cache
 docker run -p 8077:80 -d --restart=always --name report_generator_doc -t report_generator_image bash
